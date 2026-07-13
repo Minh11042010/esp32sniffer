93cd5794ec9ed47f4f1483e97a3f283d4e7a181c
Minh2010 <109939183+Minh11042010@users.noreply.github.com>
2026-07-13 13:00:31 +0700

    Update main.c

--- main/main.c
+++ main/main.c
@@ -125,8 +125,8 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      "SIGNAV"
-#define EXAMPLE_ESP_WIFI_PASS      "12345678"
+#define EXAMPLE_ESP_WIFI_SSID      "PA"
+#define EXAMPLE_ESP_WIFI_PASS      "09102010"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  20
 
 /* FreeRTOS event group to signal when we are connected*/
